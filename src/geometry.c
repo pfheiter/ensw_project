a9320e7e1f09606889fe88cbf0faff701f218048
schibe01 <schibe01@thu.de>
2026-06-23 09:04:29 +0200

    Implement calc perimeter

--- src/geometry.c
+++ src/geometry.c
@@ -1,8 +1,12 @@
 #include "geometry.h"
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 double calculate_area(const Circle* circle) {
     return M_PI * circle->radius * circle->radius;
 }
 
+double calculate_perimeter(const Circle* circle) {
+    return 2 * M_PI * circle->radius;
+}
 // comment
